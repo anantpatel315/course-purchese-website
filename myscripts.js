03fea0553acea4bf8616d97063c826aa48072663
anantkumar patel <51236244+anantpatel315@users.noreply.github.com>
2021-02-19 14:34:23 +0530

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -2,3 +2,12 @@ var anant=prompt("ENTER YOUR NAME");
 
 var ap=anant.toUpperCase();
 alert("Welcome"+ap);
+function ap()
+{
+    alert("get milk");
+    alert("push wife boobs");
+    alert("enter penies in to wife vagiena");
+    alert("enjoy sex");
+    alert("but first warning to use condom after sex");
+}
+ap();
