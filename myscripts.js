e7213280952d027e42cc77be7e4fcdb26d6e2bb0
anantkumar patel <51236244+anantpatel315@users.noreply.github.com>
2021-02-19 14:37:11 +0530

    Update myscripts.js

--- myscripts.js
+++
@@ -1,9 +0,0 @@
-function ap()
-{
-    alert("get milk");
-    alert("push wife boobs");
-    alert("enter penies in to wife vagiena");
-    alert("enjoy sex");
-    alert("but first warning to use condom after sex");
-}
-ap();
